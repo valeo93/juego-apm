788664d81cd3ef0c1ca6bb79a474a106b5b449d1
valeo93 <139409283+valeo93@users.noreply.github.com>
2023-10-23 21:02:30 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
+var puntos=0
+var vidas=5
 function setup()
 {
     canvas=createCanvas(400,400)
     background("red")
-    jugador=createSprite(200,200,50,100)
+    jugador=createSprite(200,350,50,100)
+    malos=createGroup()
+    buenos=createGroup()
+    bordes=createEdgeSprites()
 
 }
 function draw()
 {
-    background("red")
+    background("green")
+    jugador.collide(bordes)
+    jugador.collide(malos,perder_vidas)
+    jugador.collide(buenos,ganar_puntos)
+    mostrar_marcadores()
+    if(frameCount%50==0)
+    {
+        malo_1=createSprite(random(0,400),-10,30,30)
+        malo_1.velocity.y=5
+        malo_1.shapeColor="yellow"
+        malos.add(malo_1)
+    }
+    if(frameCount%70==0)
+    {
+        bueno_1=createSprite(random(0,400),-10,30,30)
+        bueno_1.velocity.y=5
+        bueno_1.shapeColor="red"
+        buenos.add(bueno_1)
+    }
     drawSprites()
 }
 function Mover_derecha()
@@ -17,4 +40,26 @@ function Mover_derecha()
 function Mover_izquierda()
 {
     jugador.x=jugador.x -5
+}
+function perder_vidas()
+{
+    vidas=vidas-1
+    if(vidas==0)
+    {
+        jugador.destroy()
+        textSize(30)
+        text("¡PERDISTE!", 150,200)
+        noLoop()
+    }
+}
+function ganar_puntos()
+{
+    puntos=puntos+1
+}
+function mostrar_marcadores()
+{
+    textSize(20)
+    fill("black")
+    text(puntos + " puntos", 300,50 )
+    text(vidas + " vidas", 10,50)
 }
